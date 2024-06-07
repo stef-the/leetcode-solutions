b5da8e425046e36592955f09343b5cb1942bf0c1
stef-the <75397677+stef-the@users.noreply.github.com>
2024-06-07 22:32:43 +0100

    small improvement using dict (avoiding int conversion)

--- solutions/letter-combinations-of-a-phone-number.py
+++ solutions/letter-combinations-of-a-phone-number.py
@@ -1,23 +1,12 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        charmap = [
-            (),
-            (),
-            ("a","b","c"),
-            ("d","e","f"),
-            ("g","h","i"),
-            ("j","k","l"),
-            ("m","n","o"),
-            ("p","q","r","s"),
-            ("t","u","v"),
-            ("w","x","y","z")
-        ]
+        charmap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         if digits == "": return ""
-        output = [x for x in charmap[int(digits[0])]]
+        output = [x for x in charmap[digits[0]]]
         for i in list(digits)[1:]:
-            output = [list(output) for x in range(len(charmap[int(i)]))]
+            output = [list(output) for x in range(len(charmap[i]))]
             for j in range(len(output)):
                 for k in range(len(output[j])):
-                    output[j][k] += charmap[int(i)][j]
+                    output[j][k] += charmap[i][j]
             output = list(chain.from_iterable(output))
         return output
