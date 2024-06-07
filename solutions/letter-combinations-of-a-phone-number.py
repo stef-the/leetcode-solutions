687a0f94235fd40949b3e315291ea286f2f4d95d
stef-the <75397677+stef-the@users.noreply.github.com>
2024-06-07 22:38:32 +0100

    major improvement using recursion

--- solutions/letter-combinations-of-a-phone-number.py
+++ solutions/letter-combinations-of-a-phone-number.py
@@ -1,12 +1,13 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         charmap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        if digits == "": return ""
-        output = [x for x in charmap[digits[0]]]
-        for i in list(digits)[1:]:
-            output = [list(output) for x in range(len(charmap[i]))]
-            for j in range(len(output)):
-                for k in range(len(output[j])):
-                    output[j][k] += charmap[i][j]
-            output = list(chain.from_iterable(output))
+        if not digits: return
+        output = []
+
+        def backtrack(n, c):
+            if not n:
+                return output.append(c)
+            [backtrack(n[1:], c + letter) for letter in charmap[n[0]]]
+
+        backtrack(digits, "")
         return output
